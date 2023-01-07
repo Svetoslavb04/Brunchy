4239720d8079e991935972579d6ab994f9732bda
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-07 11:50:48 +0200

    feat: add basket list

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,43 @@
 import styles from './App.module.scss';
+
+import { useState } from 'react';
+
 import Navigation from '../Navigation/Navigation';
 import Headline from '../Headline/Headline';
 import Subheadline from '../Subheadline/Subheadline';
 import MealsList from '../MealsList/MealsList';
 import MealsBasket from '../MealsBasket/MealsBasket';
-import { useState } from 'react';
+import BasketListingModal from '../BasketListingModal/BasketListingModal';
 
 function App() {
 
   const [basket, setBasket] = useState([]);
+
   const [isBasketModalVisible, setIsBasketModalVisible] = useState(false);
+  const handleAppClick = (e) => {
+
+    const target = e.target;
+
+    if (
+      isBasketModalVisible
+      && !target.closest('.meals-basket')
+      && !target.closest('.basket-list')
+    ) {
+      setIsBasketModalVisible(false)
+    }
+  }
 
   const addToBasket = (meal) => setBasket(currentMeals => [...currentMeals, meal]);
+  const addToBasket = (meal) => setBasket(currentMeals => [...currentMeals, { ...meal, id: currentMeals.length }]);
 
   const openBasketModal = () => setIsBasketModalVisible(true);
 
   return (
     <div className={[styles['App']]}>
+    <div
+      className={[styles['App']]}
+      onClick={handleAppClick}
+    >
       <Navigation />
       <Headline />
       <Subheadline />
@@ -25,6 +46,16 @@ function App() {
         basket={basket}
         openBasketModal={openBasketModal}
       />
+      <div id={styles['basket-wrapper']}>
+        <BasketListingModal
+          basket={basket}
+          isVisible={isBasketModalVisible}
+        />
+        <MealsBasket
+          basket={basket}
+          openBasketModal={openBasketModal}
+        />
+      </div>
     </div>
   );
 }
